14a6ad11ea08fa69c9ac5a513abb6bc3ff43e50f
terutora <tachi877teru@gmail.com>
2024-06-28 23:01:40 +0900

    初期設定を行いました

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    images: {
-      domains: ['localhost'], // Strapiの画像を使用する場合はここにドメインを追加
-    },
-  }
-  
-  module.exports = nextConfig
